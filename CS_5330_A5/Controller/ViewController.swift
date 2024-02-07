10b60f9cbebd9002f0f7eae4d72af7748500fcf4
Kyle Zimmerman <kzdesignsios@gmail.com>
2024-02-07 08:44:07 -0800

    Added IBOutlets and actions to ViewController

--- CS_5330_A5/Controller/ViewController.swift
+++ CS_5330_A5/Controller/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var backGroundImage: UIImageView!
+    @IBOutlet weak var header: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var rewardImage: UIImageView!
+    @IBOutlet weak var missionLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func setupUI() {
+        // sets up UI
+    }
+    
+    @IBAction func buttonOneAction(_ sender: Any) {
+    }
+    
+    @IBAction func buttonTwoAction(_ sender: Any) {
+    }
+    
 
 }
 
